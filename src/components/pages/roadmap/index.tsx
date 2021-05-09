4b64ad4f05cdea6da2d7e69ad6ec4c9fe0962a50
Jaime Jesús Serrano <jaimejesusserrano@gmail.com>
2021-05-09 22:12:15 +0200

    [ADD] RoadmapMilestone service and React Query

--- src/components/pages/roadmap/index.tsx
+++ src/components/pages/roadmap/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+
+import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 
 import MainTemplate from 'components/templates/main'
-import RoadmapMilestone from 'types/RoadmapMilestone'
+import { getById as getByIdRoadmapMillestone } from 'services/roadmap-milestone'
 
 import Board from './components/board'
 import { SBoardWrapper, SWrapper } from './styles'
@@ -14,95 +16,13 @@ interface ParamTypes {
 const Roadmap = (): JSX.Element => {
   const { id } = useParams<ParamTypes>()
 
-  const roadmapMilestone: RoadmapMilestone = {
-    milestones: [
-      {
-        goals: [
-          {
-            category: {
-              name: 'Infrastructure',
-              color: 'red',
-            },
-            description: '',
-            finishDate: new Date('2021-04-04'),
-            images: [],
-            name: 'Create repository',
-            shortDescription: '',
-            startDate: new Date('2021-03-13'),
-            status: 'Done',
-            tags: [],
-          },
-          {
-            category: {
-              name: 'Infrastructure',
-              color: 'red',
-            },
-            description: '',
-            finishDate: new Date('2021-04-04'),
-            images: [],
-            name: 'Install ReactJS and basic structure',
-            shortDescription: '',
-            startDate: new Date('2021-04-04'),
-            status: 'Done',
-            tags: [],
-          },
-          {
-            category: {
-              name: 'Infrastructure',
-              color: 'red',
-            },
-            description: '',
-            finishDate: new Date('2021-04-18'),
-            images: [],
-            name: 'Define initial Roadmap types',
-            shortDescription: '',
-            startDate: new Date('2021-04-04'),
-            status: 'Done',
-            tags: [],
-          },
-          {
-            category: {
-              name: 'Components',
-              color: 'green',
-            },
-            description: 'This feature indcludes responsive styles basic html',
-            finishDate: new Date('2021-05-01'),
-            images: [],
-            name: 'Create board structure v1',
-            shortDescription: '',
-            startDate: new Date('2021-04-18'),
-            status: 'Done',
-            tags: [],
-          },
-          {
-            category: {
-              name: 'Infrastructure',
-              color: 'red',
-            },
-            description:
-              'With this feature we will can get a fake data from a fake service. It is necessary to future real connections.',
-            images: [],
-            name: 'Add services and repositories structure',
-            shortDescription: '',
-            startDate: new Date('2021-05-01'),
-            status: 'Ongoing',
-            tags: [],
-          },
-        ],
-        name: '0.1',
-        startDate: new Date('2021-03-13'),
-        status: 'Ongoing',
-      },
-    ],
-    name: 'Roadmap Milestone',
-    startDate: new Date('2021-03-13'),
-  }
+  const { isLoading, isError, data, error } = useQuery('roadmapMilestone', () => getByIdRoadmapMillestone(parseInt(id)))
 
   return (
     <MainTemplate>
       <SWrapper>
         <SBoardWrapper>
-          <Board roadmapMilestone={roadmapMilestone} />
+          <Board roadmapMilestone={data} />
         </SBoardWrapper>
       </SWrapper>
     </MainTemplate>
