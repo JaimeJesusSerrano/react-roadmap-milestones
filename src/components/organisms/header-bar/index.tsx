092163f751cc934244d0c631add5e4da91b60553
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2021-11-09 22:32:06 +0100

    [UPDATE] Header temporal title

--- src/components/organisms/header-bar/index.tsx
+++ src/components/organisms/header-bar/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import * as S from './styled'
 
 const HeaderBar = (): JSX.Element => {
-  return <S.Wrapper>Header bar</S.Wrapper>
+  return <S.Wrapper>React Roadmap Milestones component under construction</S.Wrapper>
 }
 
 export default HeaderBar
