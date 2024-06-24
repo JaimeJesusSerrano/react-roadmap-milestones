73de00f482a4e68d0a643aed29571ec13a329f5a
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2024-06-24 23:55:19 +0200

    fix: storybook config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,14 @@
-import { type Preview } from "@storybook/react";
+import { type Preview } from '@storybook/react'
 
 export const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+        date: /Date$/i
+      }
+    }
+  }
 }
+
+export default preview
